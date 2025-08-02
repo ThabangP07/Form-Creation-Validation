00e6a2b61a845f08c4daa48ee5e313de8b57564a
Thabang <phoshokothabang1@gmail.com>
2025-08-02 09:39:05 +0200

    feat: fetch data from the api and convert it to json format

--- fetch-data.js
+++ fetch-data.js
@@ -2,4 +2,11 @@ const fetchUserData = async () => {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
     const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+    } catch (error) {
+        console.error(error)
+    }
 }
