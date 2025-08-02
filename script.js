cd0b1ac3749d370f07d82e55aeceed22d0a02279
Thabang <phoshokothabang1@gmail.com>
2025-08-02 08:07:13 +0200

    feat: prevent default form submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
     const feedbackDiv = getElementById('form-feedback');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    })
 });
