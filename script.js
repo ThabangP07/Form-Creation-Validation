b1b8dc8196821bb71ee8385fd1339c09fb884148
Thabang <phoshokothabang1@gmail.com>
2025-08-02 08:19:57 +0200

    feat: retrieve and trim form input values

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
-    const feedbackDiv = getElementById('form-feedback');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
     })
+
+    
 });
